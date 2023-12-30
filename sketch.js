b3ffc5836bc0224cb0fcf6d6ea0413a9b681aa67
releasethefishys <104481700+releasethefishys@users.noreply.github.com>
2023-12-29 23:25:53 -0800

    Update sketch.js
    Add a button

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
   // Starts in the middle
   x = width / 2;
   y = height;
+  setup2();
 }
 
 function draw() {
@@ -27,3 +28,37 @@ function draw() {
   }
 }
 
+let input, button, greeting;
+
+function setup2() {
+  // create canvas
+ // createCanvas(710, 400);
+
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'what is your name?');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function greet() {
+  const name = input.value();
+  greeting.html('hello ' + name + '!');
+  input.value('');
+
+  for (let i = 0; i < 200; i++) {
+    push();
+    fill(random(255), 255, 255);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(name, 0, 0);
+    pop();
+  }
+}
